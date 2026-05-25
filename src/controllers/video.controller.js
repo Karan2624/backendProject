7e1c767f649e4b7f39da9b84cb3b0c1806f865cc
Karan Kumar Gupta <karangupta98335@gmail.com>
2026-05-25 14:53:03 +0530

    updated

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -41,4 +41,9 @@ const publishVideo = asyncHandler(async(req,res) => {
     )
 });
 
+const getAllVideo = asyncHandler(async(req,res) => {
+    const {page = 1,limit = 10,query,sortBy,sortType,userId} = req.query;
+    
+})
+
 export {publishVideo};
